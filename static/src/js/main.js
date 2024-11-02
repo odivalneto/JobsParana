3d23faddb3d67eaeb1b947ccd14d8098646900b2
Odival Neto <odivalneto@gmail.com>
2024-11-02 13:36:02 -0300

    Updates componentes css

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,13 +1,2 @@
-const search_input = document.querySelector("#search-input");
 
-search_input.addEventListener("input", (e) => {
-    e.preventDefault();
-    fetch('/jobs/?search=' + e.target.value, {
-        method: 'GET',
-        mode: 'cors',
-
-    }).then(res => res.ok).then(data => {
-        console.log(data)
-    })
-})
 
